98ec8667ec8d3c7e6586cc3734f367a077f53b3f
Paul Schifferer <paul@schifferers.net>
2025-05-18 19:18:54 -0700

    Calculator handler and tests

--- Sources/HorologyAPI/Services/HorologyService+Calculate.swift
+++ Sources/HorologyAPI/Services/HorologyService+Calculate.swift
@@ -5,16 +5,55 @@
 //
 
 import Foundation
+import HorologyCore
 import OpenAPIRuntime
 import OpenAPIVapor
 import Vapor
 
 extension HorologyService {
-      func calculate(_ input: Operations.Calculate.Input) async throws -> Operations.Calculate.Output
-    {
+    func calculate(_ input: Operations.Calculate.Input) async throws -> Operations.Calculate.Output {
+        guard case .json(let body) = input.body
+        else {
+            return .badRequest(
+                .init(
+                    body: .json(
+                        .init(
+                            code: ErrorCodes.serialization.rawValue,
+                            reason: "Request body could not be deserialized")))
+            )
+        }
 
+        let calendar: Calendar
+        do {
+            calendar = try Calendar.from(identifier: body.calendar ?? "gregorian")
+        } catch {
+            // req.logger.error("Failed to create calendar: \(error)")
+            return .badRequest(
+                .init(
+                    body: .json(
+                        .init(
+                            code: ErrorCodes.invalidCalendar.rawValue,
+                            reason: "Invalid calendar identifier"))))
+        }
 
-        let response = Components.Schemas.CalculationResponse()
+        let startDate = makeDateFields(from: body.date)
+        let adjustments = makeDateFields(from: body.adjustments)
+
+        guard let mode = DateTimeMode(rawValue: body.mode.rawValue) else {
+            throw Abort(.badRequest, reason: "Invalid mode")
+        }
+
+        let calculator = DateCalculator(
+            startDate: startDate,
+            adjustments: adjustments,
+            calendar: calendar)
+        let result = try calculator.calculateDate(with: mode)
+
+        let response = Components.Schemas.CalculationResponse(
+            date: result.formatted(.iso8601),
+            components: makeContentFromDateComponents(
+                calendar.dateComponents(
+                    [.year, .month, .day, .hour, .minute, .second], from: result)))
         return .ok(.init(body: .json(response)))
     }
 }
--- Tests/HorologyAPITests/CalculateTests.swift
+++ Tests/HorologyAPITests/CalculateTests.swift
@@ -13,118 +13,98 @@ import VaporTesting
 @Suite
 struct CalculateTests {
 
-//     private let fromDate = HorologyAPI.DateFields(
-//         year: DateTimeField(
-//             type: DateTimeFieldType.year.rawValue, value: 2023),
-//         month: DateTimeField(
-//             type: DateTimeFieldType.month.rawValue, value: 10),
-//         day: DateTimeField(
-//             type: DateTimeFieldType.day.rawValue, value: 1),
-//         hour: DateTimeField(
-//             type: DateTimeFieldType.hour.rawValue, value: 0),
-//         minute: DateTimeField(
-//             type: DateTimeFieldType.minute.rawValue, value: 0),
-//         second: DateTimeField(
-//             type: DateTimeFieldType.second.rawValue, value: 0))
-//     private let adjustments = HorologyAPI.DateFields(
-//         year: DateTimeField(
-//             type: DateTimeFieldType.year.rawValue, value: 1),
-//         month: DateTimeField(
-//             type: DateTimeFieldType.month.rawValue, value: 0),
-//         day: DateTimeField(type: DateTimeFieldType.day.rawValue, value: 0),
-//         hour: DateTimeField(
-//             type: DateTimeFieldType.hour.rawValue, value: 0),
-//         minute: DateTimeField(
-//             type: DateTimeFieldType.minute.rawValue, value: 0),
-//         second: DateTimeField(
-//             type: DateTimeFieldType.second.rawValue, value: 0))
-
-//     private func withApp(_ test: (Application) async throws -> Void) async throws {
-//         let app = try await Application.make(.testing)
-//         do {
-//             try await configure(app)
-//             try await test(app)
-//         } catch {
-//             try await app.asyncShutdown()
-//             throw error
-//         }
-//         try await app.asyncShutdown()
-//     }
+    private let fromDate = Components.Schemas.DateFields(
+        year: Components.Schemas.DateTimeField(
+            _type: DateTimeFieldType.year.rawValue, value: 2023),
+        month: Components.Schemas.DateTimeField(
+            _type: DateTimeFieldType.month.rawValue, value: 10),
+        day: Components.Schemas.DateTimeField(
+            _type: DateTimeFieldType.day.rawValue, value: 1),
+        hour: Components.Schemas.DateTimeField(
+            _type: DateTimeFieldType.hour.rawValue, value: 0),
+        minute: Components.Schemas.DateTimeField(
+            _type: DateTimeFieldType.minute.rawValue, value: 0),
+        second: Components.Schemas.DateTimeField(
+            _type: DateTimeFieldType.second.rawValue, value: 0))
+    private let adjustments = Components.Schemas.DateFields(
+        year: Components.Schemas.DateTimeField(
+            _type: DateTimeFieldType.year.rawValue, value: 1),
+        month: Components.Schemas.DateTimeField(
+            _type: DateTimeFieldType.month.rawValue, value: 0),
+        day: Components.Schemas.DateTimeField(_type: DateTimeFieldType.day.rawValue, value: 0),
+        hour: Components.Schemas.DateTimeField(
+            _type: DateTimeFieldType.hour.rawValue, value: 0),
+        minute: Components.Schemas.DateTimeField(
+            _type: DateTimeFieldType.minute.rawValue, value: 0),
+        second: Components.Schemas.DateTimeField(
+            _type: DateTimeFieldType.second.rawValue, value: 0))
 
     @Test
     func testCalculate() async throws {
+
         let handler = HorologyService()
-        // let response = try await handler.calculate( .init())
-//         try await withApp { app in
-//             try await app.testing().test(
-//                 .GET, "calculate",
-//                 beforeRequest: { req in
-//                     try req.content.encode(
-//                         CalculationRequest(
-//                             date: fromDate,
-//                             adjustments: adjustments,
-//                             calendar: "gregorian",
-//                             mode: DateTimeMode.dateAndTime.rawValue))
-//                 },
-//                 afterResponse: { res async throws in
-//                     #expect(res.status == .ok)
-//                     #expect(res.headers.contentType == .json)
-//                     let resp = try res.content.decode(CalculationResponse.self)
-//                     #expect(resp.date == "2024-10-01T07:00:00Z")
-//                     #expect(resp.components.year == 2024)
-//                     #expect(resp.components.month == 10)
-//                     #expect(resp.components.day == 1)
-//                     #expect(resp.components.hour == 0)
-//                     #expect(resp.components.minute == 0)
-//                     #expect(resp.components.second == 0)
-//                 })
-//         }
+        let response = try await handler.calculate(
+            body: .json(
+                .init(
+                    date: fromDate,
+                    adjustments: adjustments, calendar: "gregorian",
+                    mode: Components.Schemas.CalculationRequest.ModePayload.dateAndTime)))
+
+        guard case .json(let resp) = try response.ok.body else {
+            Issue.record("Unexpected response body")
+            return
+        }
+
+        #expect(resp.date == "2024-10-01T07:00:00Z")
+        #expect(resp.components.year == 2024)
+        #expect(resp.components.month == 10)
+        #expect(resp.components.day == 1)
+        #expect(resp.components.hour == 0)
+        #expect(resp.components.minute == 0)
+        #expect(resp.components.second == 0)
     }
 
-//     @Test
-//     func testInvalidCalendarIdentifier() async throws {
-//         try await withApp { app in
-//             try await app.testing().test(
-//                 .GET, "calculate",
-//                 beforeRequest: { req in
-//                     try req.content.encode(
-//                         CalculationRequest(
-//                             date: fromDate,
-//                             adjustments: adjustments,
-//                             calendar: "invalid_calendar",
-//                             mode: "dateAndTime"))
-//                 },
-//                 afterResponse: { res async throws in
-//                     #expect(res.status == .badRequest)
-//                     #expect(res.headers.contentType == .json)
-//                     let resp = try res.content.decode(ErrorResponse.self)
-//                     #expect(resp.error == true)
-//                     #expect(resp.reason == "Invalid calendar identifier")
-//                 })
-//         }
-//     }
+    @Test
+    func testInvalidCalendarIdentifier() async throws {
 
-//     @Test
-//     func testCalculateInvalidMode() async throws {
-//         try await withApp { app in
-//             try await app.testing().test(
-//                 .GET, "calculate",
-//                 beforeRequest: { req in
-//                     try req.content.encode(
-//                         CalculationRequest(
-//                             date: fromDate,
-//                             adjustments: adjustments,
-//                             calendar: "gregorian",
-//                             mode: "invalid_mode"))
-//                 },
-//                 afterResponse: { res async throws in
-//                     #expect(res.status == .badRequest)
-//                     #expect(res.headers.contentType == .json)
-//                     let resp = try res.content.decode(ErrorResponse.self)
-//                     #expect(resp.error == true)
-//                     #expect(resp.reason == "Invalid mode")
-//                 })
-//         }
-//     }
+        let handler = HorologyService()
+        let response = try await handler.calculate(
+            body: .json(
+                .init(
+                    date: fromDate,
+                    adjustments: adjustments, calendar: "invalid",
+                    mode: Components.Schemas.CalculationRequest.ModePayload.dateAndTime)))
+
+        guard case .json(let resp) = try response.badRequest.body else {
+            Issue.record("Response should have returned an error")
+            return
+        }
+
+        #expect(resp.code == ErrorCodes.invalidCalendar.rawValue)
+        #expect(resp.reason == "")
+    }
+
+    @Test
+    func testCalculateInvalidMode() async throws {
+        //         try await withApp { app in
+        //             try await app.testing().test(
+        //                 .GET, "calculate",
+        //                 beforeRequest: { req in
+        //                     try req.content.encode(
+        //                         CalculationRequest(
+        //                             date: fromDate,
+        //                             adjustments: adjustments,
+        //                             calendar: "gregorian",
+        //                             mode: "invalid_mode"))
+        //                 },
+        //                 afterResponse: { res async throws in
+        //                     #expect(res.status == .badRequest)
+        //                     #expect(res.headers.contentType == .json)
+        //                     let resp = try res.content.decode(ErrorResponse.self)
+        //                     #expect(resp.error == true)
+        //                     #expect(resp.reason == "Invalid mode")
+        //                 })
+        //         }
+    }
 
 }
